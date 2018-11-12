39fa6246af4b4836fcb708d67a5ef3867a95112f
李业恒 <liyh1928@chinaunicom.cn>
2018-11-12 15:35:50 +0800

    尚学堂视频 spring boot 学习示例

---
+++ _01springboothello/src/main/java/net/nanquanyuhao/springboothello/controller/HelloWorld.java
@@ -0,0 +1,23 @@
+package net.nanquanyuhao.springboothello.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by nanquanyuhao on 2018/11/12.
+ */
+@Controller
+public class HelloWorld {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public Map<String, Object> showHelloWorld(){
+        Map<String, Object> map = new HashMap<>();
+        map.put("msg", "HelloWorld");
+        return map;
+    }
+}

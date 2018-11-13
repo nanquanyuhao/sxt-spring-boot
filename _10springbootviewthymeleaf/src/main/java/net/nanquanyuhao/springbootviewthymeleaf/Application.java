26c0d8a28a074c1751034fc0a4a2809e476b5785
nanquanyuhao <nanquanyuhao@foxmail.com>
2018-11-13 13:54:05 +0800

    项目搭建及基础的字符串操作

---
+++ _10springbootviewthymeleaf/src/main/java/net/nanquanyuhao/springbootviewthymeleaf/Application.java
@@ -0,0 +1,15 @@
+package net.nanquanyuhao.springbootviewthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Thymeleaf 入门案例
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
---
+++ _10springbootviewthymeleaf/src/main/java/net/nanquanyuhao/springbootviewthymeleaf/controller/DemoController.java
@@ -0,0 +1,20 @@
+package net.nanquanyuhao.springbootviewthymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by nanquanyuhao on 2018/11/13.
+ *
+ * Thymeleaf 入门案例
+ */
+@Controller
+public class DemoController {
+
+    @RequestMapping("/show")
+    public String showInfo(Model model){
+        model.addAttribute("msg", "Thymeleaf 第一个案例");
+        return "index";
+    }
+}

545b010fd892b93c35139a585bccfdcf704ae2df
nanquanyuhao <nanquanyuhao@foxmail.com>
2018-11-13 18:48:12 +0800

    thymeleaf 对日期处理的示例

--- _10springbootviewthymeleaf/src/main/java/net/nanquanyuhao/springbootviewthymeleaf/controller/DemoController.java
+++ _10springbootviewthymeleaf/src/main/java/net/nanquanyuhao/springbootviewthymeleaf/controller/DemoController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Date;
+
 /**
  * Created by nanquanyuhao on 2018/11/13.
  *
@@ -15,6 +17,7 @@ public class DemoController {
     @RequestMapping("/show")
     public String showInfo(Model model){
         model.addAttribute("msg", "Thymeleaf 第一个案例");
+        model.addAttribute("key", new Date());
         return "index";
     }
 }

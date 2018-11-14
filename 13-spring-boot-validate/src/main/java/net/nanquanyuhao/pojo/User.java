e414248d0207f66b805e9fccff0e19f28b7a2f6b
nanquanyuhao <nanquanyuhao@foxmail.com>
2018-11-14 17:59:12 +0800

    加入基本校验规则的示例

--- 13-spring-boot-validate/src/main/java/net/nanquanyuhao/pojo/User.java
+++ 13-spring-boot-validate/src/main/java/net/nanquanyuhao/pojo/User.java
@@ -1,6 +1,11 @@
 package net.nanquanyuhao.pojo;
 
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 
 /**
  * Created by nanquanyuhao on 2018/11/14.
@@ -8,11 +13,17 @@ import javax.validation.constraints.NotBlank;
 public class User {
 
     @NotBlank(message = "用户名不能为空") // 非空校验
+    @Length(min = 2, max = 6, message = "最小长度为2位，最大长度为6位")
     private String name;
 
-    @NotBlank(message = "密码不能为空") // 密码非空校验
+    @NotEmpty
     private String password;
+
+    @Min(value = 15)
     private Integer age;
+
+    @Email
+    private String email;
 
     public String getName() {
         return name;
@@ -21,6 +32,7 @@ public class User {
     public void setName(String name) {
         this.name = name;
     }
+
 
     public String getPassword() {
         return password;
@@ -36,6 +48,14 @@ public class User {
 
     public void setAge(Integer age) {
         this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     @Override

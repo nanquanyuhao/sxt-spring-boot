a5b8f6187779e0e897b5de93e4ad9b816c438b4d
nanquanyuhao <nanquanyuhao@foxmail.com>
2018-11-14 19:36:23 +0800

    @ExceptionHandler 基础示例

---
+++ 15-spring-boot-exception2/src/main/java/net/nanquanyuhao/Application.java
@@ -0,0 +1,15 @@
+package net.nanquanyuhao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringBoot处理异常方式一：自定义错误页面
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}

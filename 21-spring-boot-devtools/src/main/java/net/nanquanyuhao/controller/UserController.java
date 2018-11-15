f91f2e0484c1cdf9366b71fdb272b84cd1e08ef8
nanquanyuhao <nanquanyuhao@foxmail.com>
2018-11-15 16:03:48 +0800

    热部署的示例全部调通生效

---
+++ 21-spring-boot-devtools/src/main/java/net/nanquanyuhao/controller/UserController.java
@@ -0,0 +1,19 @@
+package net.nanquanyuhao.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by nanquanyuhao on 2018/11/15.
+ * <p>
+ * SpringBoot----DevTools
+ */
+@Controller
+public class UserController {
+
+    @RequestMapping("/show")
+    public String showPage() {
+        System.out.println("ShowPage...init...aaa");
+        return "index";
+    }
+}
